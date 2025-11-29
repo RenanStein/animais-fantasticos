ae5d36115dea28ffae1e81d54efe7f149b091369
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2025-11-29 18:21:44 -0300

    active accordion (faq)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function initTabNav(){
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
 tabContent[0].classList.add('ativo');
@@ -16,3 +17,26 @@ if(tabMenu.length && tabContent.length) {
     })
   })
 }
+}
+
+initTabNav();
+
+
+function initAccordion(){
+const accordionList = document.querySelectorAll('.js-accordion dt');
+if(accordionList.length){
+accordionList[0].classList.add('ativo');
+accordionList[0].nextElementSibling.classList.add('ativo');
+
+function activeAccordion(event) {
+  this.classList.toggle('ativo')
+  this.nextElementSibling.classList.toggle('ativo')
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener('click', activeAccordion);
+})
+}
+}
+
+initAccordion();
