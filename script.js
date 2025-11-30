6cad00afcb3f20f51bab07149bc7d5e0026c02ce
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2025-11-30 09:35:07 -0300

    animacao scroll suave

--- script.js
+++ script.js
@@ -41,14 +41,44 @@ accordionList.forEach((item) => {
 
 initAccordion();
 
+function initScrollSuave(){
 const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
 function scrollToSection(event) {
   event.preventDefault();
   const href = this.getAttribute('href');
-  console.log(href)
+  const section = document.querySelector(href);
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
 }
 
 linksInternos.forEach((link) => {
   link.addEventListener('click', scrollToSection);
 })
+}
+
+initScrollSuave();
+
+function initAnimacaoScroll(){
+const sections = document.querySelectorAll('.js-scroll');
+if(sections.length){
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll(){
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - windowMetade;
+    if(sectionTop < 0){
+      section.classList.add('ativo');
+    } else {
+      section.classList.remove('ativo');
+    }
+  })
+}
+animaScroll()
+window.addEventListener('scroll', animaScroll);
+}
+}
+
+initAnimacaoScroll();
