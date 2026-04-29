6118685fb93349f7af3e666aa25f8cc7d62d99a1
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2026-04-28 21:42:22 -0300

    animação de números
    criado função para animar números ao carregar a page.
    Alterado condição do animcao-scroll.js para remover a class somente quando existir ela.

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -5,10 +5,11 @@ const windowMetade = window.innerHeight * 0.6;
 
 function animaScroll(){
   sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top - windowMetade;
-    if(sectionTop < 0){
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = (sectionTop - windowMetade) < 0;
+    if(isSectionVisible){
       section.classList.add('ativo');
-    } else {
+    } else if(section.classList.contains('ativo')){
       section.classList.remove('ativo');
     }
   })
