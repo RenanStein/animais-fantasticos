38502f781e776285da5dafbae0e8c3f24cd128fe
Renan Stein <renansteindev@gmail.com>
2026-08-06 20:54:54 -0300

    webpack
    adicionado webpack no projeto

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,5 +1,6 @@
 export default function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const windowMetade = window.innerHeight * 0.6;
 
   function animaScroll() {
     sections.forEach((section) => {
