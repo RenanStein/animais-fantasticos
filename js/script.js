8d41ee3fa6c221387507f8a8a202e8a7a253febd
Renan Stein <renansteindev@gmail.com>
2026-09-02 20:26:53 -0300

    refatoração
    módulos anima-scroll, anima-numeros, fetch-animais e fetch-bitcoin foram refatorados

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-import ScrollSuave from "./modules/scroll-suave.js";
-import Accordion from "./modules/accordion.js";
-import TabNav from "./modules/tabnav.js";
-import Modal from "./modules/modal.js";
-import initDropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
-import initFuncionamento from "./modules/funcionamento.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
-import initAnimacaoScroll from "./modules/animacao-scroll.js";
-import Tooltip from "./modules/tooltip.js";
+import ScrollSuave from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
+import TabNav from './modules/tabnav.js';
+import Modal from './modules/modal.js';
+import Tooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import fetchAnimais from './modules/fetch-animais.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
+import ScrollAnima from './modules/animacao-scroll.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -25,9 +25,13 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
-initFetchBitcoin();
-initAnimacaoScroll();
+
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
+
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
